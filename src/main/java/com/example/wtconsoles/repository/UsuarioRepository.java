cc30474a728eacbc218af7854263d1cc252052f5
Lairton da Silva <93054001+Lairtondasilva@users.noreply.github.com>
2022-06-09 16:37:13 -0300

    Update UsuarioRepository.java

--- src/main/java/com/example/wtconsoles/repository/UsuarioRepository.java
+++ src/main/java/com/example/wtconsoles/repository/UsuarioRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.wtconsoles.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	public Optional<Usuario> findByEmail (String email);
